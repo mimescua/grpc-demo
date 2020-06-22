200d315b9ef36446135edafed6fe45f2ef102773
mimescua <mescua@outlook.com>
2020-06-22 08:07:50 -0500

    full server streaming grpc

--- grpc-static/client/client.js
+++ grpc-static/client/client.js
@@ -1,17 +1,18 @@
 var greets = require('../server/protos/greet_pb')
 var service = require('../server/protos/greet_grpc_pb')
-var summing = require('../server/protos/sum_pb')
-var sum_service = require('../server/protos/sum_grpc_pb')
+var calc = require('../server/protos/calculator_pb')
+var calc_service = require('../server/protos/calculator_grpc_pb')
+
 var grpc = require('grpc')
 
-function callGreetings(){
+function callGreetings() {
     var client = new service.GreetServiceClient(
         'localhost:50051',
         grpc.credentials.createInsecure()
     )
 
     // create our request
-    var request =  new greets.GreetRequest()
+    var request = new greets.GreetRequest()
 
     // create a protocol buffer greeting message
     var greeting = new greets.Greeting()
@@ -22,7 +23,7 @@ function callGreetings(){
     request.setGreeting(greeting)
 
     client.greet(request, (error, response) => {
-        if(!error){
+        if (!error) {
             console.log("GreeetingResponse: ", response.getResult());
         }
         else {
@@ -30,20 +31,53 @@ function callGreetings(){
         }
     })
 }
-
-function callSum(){
-    var client = new sum_service.SumServiceClient(
+function callGreetManyTimes() {
+    var client = new service.GreetServiceClient(
         'localhost:50051',
         grpc.credentials.createInsecure()
     )
 
-    var sum_request =  new summing.SumRequest()
-    
+    //create request
+    var request = new greets.GreetManyTimesRequest()
+    var greeting = new greets.Greeting()
+    greeting.setFirstName('Michael')
+    greeting.setLastName('Jackson')
+
+    request.setGreeting(greeting)
+
+    var call = client.greetManyTimes(request, () => { })
+
+    call.on('data', (response) => {
+        console.log('Client Streaming Response: ', response.getResult());
+    })
+
+    call.on('status', (status) => {
+        console.log(status.details);
+    })
+
+    call.on('error', (error) => {
+        console.error(error.details);
+    })
+
+    call.on('end', () => {
+        console.log('Streaming ended!');
+    })
+
+}
+
+function callSum() {
+    var client = new calc_service.CalculatorServiceClient(
+        'localhost:50051',
+        grpc.credentials.createInsecure()
+    )
+
+    var sum_request = new calc.SumRequest()
+
     sum_request.setAddendOne(10)
     sum_request.setAddendTwo(15)
 
     client.sum(sum_request, (error, response) => {
-        if(!error){
+        if (!error) {
             console.log(sum_request.getAddendOne() + " + " + sum_request.getAddendTwo() + " = " + response.getSumResult());
         }
         else {
@@ -51,10 +85,42 @@ function callSum(){
         }
     })
 }
+function callPrimeNumberDecomposition() {
+    var client = new calc_service.CalculatorServiceClient(
+        "localhost:50051",
+        grpc.credentials.createInsecure()
+    );
 
-function main(){
-    console.log('Hello from client')
-    callGreetings()
-    callSum()
+    var request = new calc.PrimeNumberDecompositionRequest();
+
+    var number = 567890
+
+    request.setNumber(number);
+
+    var call = client.primeNumberDecomposition(request, () => { });
+
+    call.on("data", response => {
+        console.log("Prime Factors Found: ", response.getPrimeFactor());
+    });
+
+    call.on("error", error => {
+        console.error(error);
+    });
+
+    call.on("status", status => {
+        console.log(status);
+    });
+
+    call.on("end", () => {
+        console.log("Streaming Ended!");
+    });
+}
+
+function main() {
+    //console.log('Hello from client')
+    //callGreetings()
+    //callGreetManyTimes()
+    //callSum()
+    callPrimeNumberDecomposition()
 }
 main()
--- grpc-static/server/index.js
+++ grpc-static/server/index.js
@@ -1,11 +1,11 @@
 var greets = require('../server/protos/greet_pb')
 var service = require('../server/protos/greet_grpc_pb')
-var summing = require('../server/protos/sum_pb')
-var sum_service = require('../server/protos/sum_grpc_pb')
+var calc = require('../server/protos/calculator_pb')
+var calc_service = require('../server/protos/calculator_grpc_pb')
 
 var grpc = require('grpc')
 
-function greet(call, callback){
+function greet(call, callback) {
     var greeting = new greets.GreetResponse()
 
     greeting.setResult(
@@ -14,8 +14,23 @@ function greet(call, callback){
 
     callback(null, greeting)
 }
-function sum(call, callback){
-    var sum_response = new summing.SumResponse()
+function greetManyTimes(call, callback) {
+    var firstName = call.request.getGreeting().getFirstName()
+
+    let count = 0, intervalID = setInterval(function () {
+        var greetManyTimesResponse = new greets.GreetManyTimesResponse()
+        greetManyTimesResponse.setResult(firstName)
+
+        // setup streaming
+        call.write(greetManyTimesResponse)
+        if (++count > 9) {
+            clearInterval(intervalID)
+            call.end() // we have sent all messages!
+        }
+    }, 1000)
+}
+function sum(call, callback) {
+    var sum_response = new calc.SumResponse()
 
     sum_response.setSumResult(
         call.request.getAddendOne() + call.request.getAddendTwo()
@@ -23,11 +38,36 @@ function sum(call, callback){
 
     callback(null, sum_response)
 }
+//primeFactor -
+function primeNumberDecomposition(call, callback) {
+    var number = call.request.getNumber();
+    var divisor = 2;
+
+    console.log("Received number: ", number);
+
+    while (number > 1) {
+        if (number % divisor === 0) {
+            var primeNumberDecompositionResponse = new calc.PrimeNumberDecompositionResponse();
+
+            primeNumberDecompositionResponse.setPrimeFactor(divisor);
+
+            number = number / divisor;
+
+            //write the message using call.write()
+            call.write(primeNumberDecompositionResponse);
+        } else {
+            divisor++;
+            console.log("Divisor has increased to ", divisor);
+        }
+    }
+
+    call.end(); // all messages sent! we are done
+}
 
 function main() {
     var server = new grpc.Server()
-    server.addService(service.GreetServiceService, {greet: greet})
-    server.addService(sum_service.SumServiceService, {sum: sum})
+    server.addService(service.GreetServiceService, {greet: greet, greetManyTimes: greetManyTimes})
+    server.addService(calc_service.CalculatorServiceService, { sum: sum, primeNumberDecomposition: primeNumberDecomposition })
 
     server.bind("127.0.0.1:50051", grpc.ServerCredentials.createInsecure())
     server.start()
